0ab413856f2f700e86c8c4e9137684affc8c3b8a
widiamka <43339745+widiamka@users.noreply.github.com>
2018-09-23 10:12:30 +0700

    Add files via upload

---
+++ PBO3_10117082_Latihan2_DeklarasiVariabel/src/pbo3_10117082_latihan2_deklarasivariabel/PBO3_10117082_Latihan2_DeklarasiVariabel.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117082_latihan2_deklarasivariabel;
+
+/**
+ *
+ * @author Nama : WIDIAMEGA K
+ *         Kelas : IF3
+ *         NIM    : 10117082
+ */
+public class PBO3_10117082_Latihan2_DeklarasiVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         // Deklarasi variabel
+    int nilaiInt;
+    final double PHI = 3.14; //konstanta uppercase
+    boolean nilaiLogika;
+    char nilaiKarakter;
+    
+    //memberi nilai ke variabel
+    nilaiInt = 78;
+    nilaiLogika = false;
+    nilaiKarakter = 'D';
+    
+    //menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);  
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel karakter = " + nilaiKarakter);
+              
+    }
+    
+}
